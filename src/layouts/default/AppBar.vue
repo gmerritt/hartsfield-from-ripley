c13733c8518d850412105828f33e6efba8c7dab8
John Crossman <johncrossman@berkeley.edu>
2023-01-17 15:32:46 -0800

    CLC-7995, 'vite' instead of vue-cli, with vue 3 scaffolding

---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app-bar flat>
+    <v-app-bar-title>
+      <v-icon icon="mdi-circle-slice-4" /> MU-TH-UR 6000
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
