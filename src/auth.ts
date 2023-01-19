d486dc8b7522bd590b158cdca90689285396a2fb
John Crossman <johncrossman@berkeley.edu>
2023-01-19 15:10:37 -0800

    CLC-7997, pinia instead of vuex; vite instead of vue-cli

---
+++ src/auth.ts
@@ -0,0 +1,45 @@
+import _ from 'lodash'
+import axios from 'axios'
+import Vue from 'vue'
+
+const goToLogin = (to: any, next: any) => {
+  next({
+    path: '/login',
+    query: {
+      error: to.query.error,
+      redirect: to.fullPath
+    }
+  })
+}
+
+export default {
+  initSession: () => {
+    return new Promise<void>(resolve => {
+      const apiBaseUrl = process.env.VUE_APP_API_BASE_URL
+      axios.get(`${apiBaseUrl}/api/my/status`).then(response => {
+        Vue.prototype.$currentUser = response.data
+        Vue.prototype.$currentUser.isLoggedIn = _.get(response.data, 'isLoggedIn', false)
+
+        axios.get(`${apiBaseUrl}/api/config`).then(response => {
+          Vue.prototype.$config = response.data
+          Vue.prototype.$config.apiBaseUrl = apiBaseUrl
+          Vue.prototype.$config.isVueAppDebugMode = _.trim(process.env.VUE_APP_DEBUG).toLowerCase() === 'true'
+
+          // Set Axios CSRF headers for non-GET requests
+          axios.defaults.headers.post['X-CSRF-Token'] = response.data.csrfToken
+          axios.defaults.headers.put['X-CSRF-Token'] = response.data.csrfToken
+          axios.defaults.headers.delete['X-CSRF-Token'] = response.data.csrfToken
+          resolve()
+        })
+      })
+    })
+  },
+  requiresAuthenticated: (to: any, from: any, next: any) => {
+    const currentUser = Vue.prototype.$currentUser
+    if (currentUser.isLoggedIn) {
+      next()
+    } else {
+      goToLogin(to, next)
+    }
+  }
+}
--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,11 +1,20 @@
+import axios from '@/plugins/axios'
 import router from '@/router'
 import type { App } from 'vue'
+import VueAnnouncer from '@vue-a11y/announcer'
 import vuetify from './vuetify'
+import { createPinia } from 'pinia'
 import { loadFonts } from './webfontloader'
 
 export function registerPlugins (app: App) {
-  loadFonts()
+  loadFonts().then(() => {})
   app
-    .use(vuetify)
+    .use(axios, {
+      baseUrl: process.env.VUE_APP_API_BASE_URL,
+      withCredentials: true
+    })
+    .use(createPinia())
     .use(router)
+    .use(VueAnnouncer)
+    .use(vuetify)
 }
