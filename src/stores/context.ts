785dee64da282bd82e9e4c072cc6c408a79c51d8
Greg Merritt <gmerritt@berkeley.edu>
2023-11-14 10:47:56 -0800

    'Gregquestion' comments re: currentUser snafu

--- src/stores/context.ts
+++ src/stores/context.ts
@@ -3,9 +3,21 @@ import { nextTick } from 'vue'
 import { putFocusNextTick } from '@/utils'
 import { defineStore } from 'pinia'
 
+// Gregquestion
+// I came here via Nostromo.vue's
+//      import Context from '@/mixins/Context'
+// which led me this file via:
+//      import {useContextStore} from '@/stores/context'
+// At the bottom of this file, VSCode was unhappy with "config" in setConfig and with "currentUser" in setCurrentUser.
+// I kind of copied from ripley a bit, adding currentUser: undefined and config: undefined to the state.
+// That makes VSCode happy, with the last two actions, but does not seem sufficient for Nostromo.vue to
+// "get hip" to currentUser.
+
 export const useContextStore = defineStore('context', {
   state: () => ({
     loading: false,
+    currentUser: undefined,
+    config: undefined
   }),
   actions: {
     loadingComplete(label?: string, focusTarget?: string) {
--- src/views/Nostromo.vue
+++ src/views/Nostromo.vue
@@ -4,6 +4,32 @@
       <button id="cas-log-in" class="btn btn-default btn-primary">Sign In via CAS</button>
     </form>
   </div>
+
+<!-- Gregquestion --><!--
+
+When I load http://localhost:8080/ in my browser, I get the "Sign in via CAS"
+button as expected, and it takes me through the CalNet test login.
+However, if I immediately come back to this front page after login,
+it appears that '<div v-if="!currentUser"...' is still the state of things,
+so I still just get the "Sign in to CAS" button.
+
+I tried flipping the v-if logic to just show me the "Hello, UID..." message,
+but, instead, the page just renders blank.
+
+Still with this reversed logic, I removed  "{{ currentUser.uid }}"
+from the "Hello, UID" line; then, the page renders as expected,
+with the flipped logic causing "Hello, UID" to appear just fine
+since it "lost" the "poison" of "{{ currentUser.uid }}".
+
+I followed @/mixins/Context to /src/mixins/Context.vue,
+which imports useContextStore from /src/stores/context.ts.
+I seemed to find some things missing there, which I tried to fix
+as described in the "Gregquestion"-flagged comment in context.ts,
+but this Nostromo.vue rendering still seems to be "poisoned" by
+apparently not having access to data's currentUser.
+
+--><!-- Gregquestion -->
+
   <div v-if="currentUser" class="pt-2">
     Hello, UID {{ currentUser.uid }}!
 
